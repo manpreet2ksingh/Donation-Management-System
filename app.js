4535a2943c6cf90b86f9ab40d1392ed97013ef09
manpreet2000sin <manpreet2000sin@gmail.com>
2021-10-15 10:41:54 +0530

    updated

--- app.js
+++ app.js
@@ -46,10 +46,10 @@ app.use('/api',mailRoutes);
 
 if(process.env.NODE_ENV === 'production')
 {
-    app.use(express.static('client/build'))
+    app.use(express.static(path.join(__dirname,"/client")));
 
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+        res.sendFile(path.join(__dirname,'/client/build','index.html'));
     })
 }
 
